bb38bdc9a449996599010a080bb287d1e024f198
Tony Robinson <tonyrobinsonmusic@gmail.com>
2013-02-01 09:03:32 -0800

    new file from Jay
    here is the extended php form before customization

--- lesson-02/index.php
+++ lesson-02/index.php
@@ -5,37 +5,39 @@
 <title>Untitled Document</title>
 </head>
 <body>
-<?php
- if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
-	 ?>
-
-<?php
-// this function calculates age based on date
+<?php 
+//check to see if the page is loading from a POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
+// if there was a POST, create a confirmation page 
+//  the age function calculates age in years based on a date passed as an argument
 function age($birthdate) {
 	return (strtotime('now') - strtotime($birthdate))/(60*60*24*365);
-    }
-        $age = age('1963-12-01');
-    
-    if ($age > 21) {
-    ?>
-    <h2 style ="color:green">You are of age!, Have fun, but drink responsibly.</h2>
-    
-    <?php
-    
-    } else {
-    ?>
-    <h2 style="color:red">Sorry, you are too young. </h2>
-    <?php
-    }
-
-    } else {
-    ?>
-    <form action="" method="post">
-    <input name="birthdate" type="text">
-    <input name="" type="submit">
-    </form>
+}	
+// assign the result of calling the age( ) function passing the input date as the argument to a variable
+$age =  age($_POST['birthdate']);
+// determine if the value of the variable is greater than 21
+if ($age > 21) {
+	// if so, display a positive result , including the age and a smiley face image
+	?>
+    <h2 style="color:green">You are <?php echo intval($age) ?>, Have fun, but drink responsibly.</h2>
+    <img src="http://nelsonaspen.com/blog/wp-content/uploads/2012/05/smile.jpg" width=100 height=100 />
     <?php
 	
+} else {
+	//otherwise display a negative result, with a frowny face
+	?>
+    <h2 style="color:red">Sorry, you are  <?php echo intval($age) ?> , too young.  How about some root beer?</h2>
+    <img src=http://ronekissrichmond.files.wordpress.com/2011/11/frown1.jpg width=100 height=100 />
+    <?php
+}
+} else {
+	// if there wasn't a POST, display an input form  using an input type of date - which is supported in Chrome
+	?>
+	<form action="" method="post">
+	<input name="birthdate" type="date">
+    <input name="" type="submit">
+	</form>
+	<?php
 }
 ?>
 </body>
